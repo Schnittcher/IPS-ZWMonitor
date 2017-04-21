27591a0f9d526a68f8b70fc0c1e12767574bc6cc
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-04-21 11:10:38 +0200

    no message

---
+++ IPS-ZWMonitorSplitter/module.php
@@ -0,0 +1,30 @@
+<?
+class IPS_ZWMonitorSplitter extends IPSModule {
+  public function Create() {
+    //Never delete this line!
+    parent::Create();
+    $this->RegisterPropertyInteger("ZW_ConfiguratorID", 0);
+    $this->RegisterPropertyInteger("ZW_GatewayID", 0);
+  }
+
+  public function ApplyChanges() {
+    //Never delete this line!
+    parent::ApplyChanges();
+  }
+  public function ForwardData($JSONString) {
+    $this->SendDebug("ForwardData JSON", $JSONString,0);
+    $data = json_decode($JSONString);
+    // Buffer decodieren und in eine Variable schreiben
+    $Buffer = utf8_decode($data->Buffer);
+    if ($Buffer  == "getConfig") {
+      $config = $this->getConfig();
+      $this->SendDataToChildren(json_encode(Array("DataID" => "{B263258A-9B90-4303-AC84-70D8DBEEF4DD}", "Buffer" => $config)));
+    }
+  }
+
+  private function getConfig() {
+      $config["ZW_ConfiguratorID"] = $this->ReadPropertyInteger("ZW_ConfiguratorID");
+      $config["ZW_GatewayID"] = $this->ReadPropertyInteger("ZW_GatewayID");
+      return $config;
+  }
+}
---
+++ IPS-ZWMonitorVisu/module.php
@@ -0,0 +1,26 @@
+<?
+class IPS_ZWMonitorSplitter extends IPSModule {
+  public function Create() {
+    //Never delete this line!
+    parent::Create();
+    //Mit ZWMonitorSplitter vebrinden
+    $this->ConnectParent("{A9EAA472-5694-49FA-8D90-1D5AC1A89915}");
+  }
+
+  public function ApplyChanges() {
+    //Never delete this line!
+    parent::ApplyChanges();
+    $this->RegisterVariableString("MeshVisu", "Mesh Visualisierung", "~HTMLBox");
+  }
+
+  public function ReceiveData($JSONString) {
+    $this->SendDebug("ReceiveData JSON", $JSONString,0);
+  }
+
+  public function getVisu() {
+    $SendData = json_encode(Array("DataID" => "{F24B2861-FD7E-4022-B02C-4D9B25233E0B}", "Buffer" => "getConfig"));
+    $this->SendDebug("getVisu SendData JSON", $SendData,0);
+    $this->SendDataToParent($SendData);
+  }
+}
+?>
