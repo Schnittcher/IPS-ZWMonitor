203049616beb1992d30e3c72f73217d189f1a232
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-04-22 10:48:14 +0200

    no message

--- IPS-ZWMonitorVisu/module.php
+++ IPS-ZWMonitorVisu/module.php
@@ -5,6 +5,7 @@ class IPS_ZWMonitorVisu extends IPSModule {
     parent::Create();
     //Mit ZWMonitorSplitter vebrinden
     $this->ConnectParent("{A9EAA472-5694-49FA-8D90-1D5AC1A89915}");
+    $this->RegisterTimer("ZW-VisuUpdate", 900000, 'ZWMVisu_getVisu($_IPS[\'TARGET\']);');
   }
 
   public function ApplyChanges() {
