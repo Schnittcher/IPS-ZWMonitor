ce78173bd42d9fe2df1ff60eb537fbb26aa37980
Kai Schnittcher <kai.schnittcher@gmail.com>
2017-04-26 13:22:25 +0200

    no message

--- IPS-ZWNodeTest/module.php
+++ IPS-ZWNodeTest/module.php
@@ -49,8 +49,8 @@ class IPS_ZWMonitorNodeTest extends IPSModule {
     foreach ($ZW_Nodes as $ZW_Node) {
       if ($ZW_Node["NodeID"] <> 1 AND !in_array($ZW_Node["NodeID"], $BatteryNodes)) {
         $ZW_NodeName = IPS_GetObject($ZW_Node["InstanceID"])["ObjectName"];
-        if (IPS_GetVariableIDByName("NodeID ".$ZW_Node["NodeID"],$this->InstanceID) == false) {
-          $this->RegisterVariableBoolean("NodeID ".$ZW_Node["NodeID"], $ZW_NodeName);
+        if (IPS_GetVariableIDByName("NodeID".$ZW_Node["NodeID"],$this->InstanceID) == false) {
+          $this->RegisterVariableBoolean("NodeID".$ZW_Node["NodeID"], $ZW_NodeName);
         }
       }
     }
